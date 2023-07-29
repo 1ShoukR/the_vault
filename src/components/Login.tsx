91ea58897353e11e22b741249e34b38a0bf2714f
Rahmin <rahminshoukoohi@gmail.com>
2023-07-29 00:31:46 -0400

    created a very basic login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,7 +4,23 @@ type Props = {}
 
 const Login = (props: Props) => {
   return (
-    <div>Login</div>
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <form action="">
+          <div>
+            <label htmlFor="email">Sign in with your Email or Username</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
