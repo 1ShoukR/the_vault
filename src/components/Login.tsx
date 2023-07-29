6af586c0db3e62ba55bc435d1dec772f26689d42
Rahmin <rahminshoukoohi@gmail.com>
2023-07-29 02:05:16 -0400

    styled and handled the code for frontend logging in

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,31 +1,72 @@
 import React, { useRef } from 'react'
+import { client } from '../utilities/client'
 
 type Props = {}
 
-const Login = (props: Props) => {
-  const email = useRef()
-  const password = useRef()
+const Login: React.FC<Props> = (props) => {
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+  const body = { email: emailRef, password: passwordRef };
 
   console.log("env", import.meta.env.VITE_API_URL);
+
+  
+  const handleClick = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    if (emailRef.current && passwordRef.current) {
+      const body = {
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      };
+      await client.post("/api/auth/login", body);
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
+    <div className="container mx-auto flex h-screen flex-col items-center justify-center px-4 md:flex-row">
+      <div className="mb-8 md:mb-0 md:mr-16">
+        <img
+          className="h-16 w-16 rounded-full object-cover"
+          src="https://www.pngitem.com/pimgs/m/24-248366_profile-clipart-generic-user-generic-profile-picture-gender.png"
+          alt=""
+        />
       </div>
       <div>
         <form action="">
-          <div>
+          <div className="mb-4">
             <label htmlFor="email">Sign in with your Email or Username</label>
-            <input type="text" name="email" id="email" />
+            <input
+              className="w-full"
+              ref={emailRef}
+              type="text"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password">Password</label>
+            <input
+              className="w-full"
+              ref={passwordRef}
+              type="password"
+              name="password"
+              id="password"
+            />
           </div>
           <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <button
+              className="rounded bg-blue-500 px-4 py-2 text-white"
+              onClick={(e) => handleClick(e)}
+            >
+              Login
+            </button>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
 export default Login
