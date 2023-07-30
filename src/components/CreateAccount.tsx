4f608c7a4dfab59dfb0b4ebe4f35921f9b6033ab
Rahmin <rahminshoukoohi@gmail.com>
2023-07-30 02:34:10 -0400

    updated created acount to match the login page

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import { useAppSelector, useAppDispatch } from "../hooks/reduxTypescriptTypes";
 import { setTestData } from "../redux/testReducer";
 import { sendCreateAccountInfo } from "../hooks/fetchHooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { CreateAccountInterface } from "../utilities/interfaces";
-
-
+import { useNavigate } from "react-router-dom";
+import { AppDispatch } from "../redux/store";
+import { setUser } from "../redux/userAccountSlice";
 
 const CreateAccount: React.FC = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate()
+  const userAuthData = useAppSelector((state) => state.userAuth);
+  console.log('userAuth', userAuthData)
   const [formData, setFormData] = useState<CreateAccountInterface>({
     username: "",
     firstname: "",
@@ -21,84 +27,107 @@ const CreateAccount: React.FC = () => {
     setFormData((prevFormData) => ({...prevFormData, [name]: value,}));
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>, ) => {
+  const handleSubmit  = async (event: React.FormEvent<HTMLFormElement>, formData: CreateAccountInterface, dispatch: AppDispatch) => {
     event.preventDefault();
     // You can dispatch the form data to Redux here using useAppDispatch and setTestData
     console.log(formData);
+    const acc = await sendCreateAccountInfo(event, formData, dispatch);
+    if (acc === true) {
+      navigate('/')
+    } else {
+      alert('something happened')
+    }
   };
 
   return (
-    <div className="text-center justify-center ">
-      <h1 className="text-3xl font-bold">Create Account</h1>
-      <form onSubmit={(event: React.FormEvent<HTMLFormElement>) => {sendCreateAccountInfo(event, formData, dispatch)}} className="mt-4">
-        <div className="flex justify-center">
-          <div className="w-80">
-            <div className="mb-4">
+    <div className="flex min-h-screen items-center justify-center bg-gray-200">
+      <div className="w-full max-w-md space-y-4 rounded-xl bg-white p-6 shadow-lg">
+        <div className="text-center">
+          <img
+            className="mx-auto h-12 w-12 rounded-full object-cover"
+            src="https://www.pngitem.com/pimgs/m/24-248366_profile-clipart-generic-user-generic-profile-picture-gender.png"
+            alt=""
+          />
+          <h2 className="mt-6 text-3xl font-bold text-gray-900">
+            Create Account
+          </h2>
+        </div>
+        <form
+          className="mt-8 space-y-6"
+          onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+            handleSubmit(event, formData, dispatch);
+          }}
+        >
+          <div className="-space-y-px rounded-md">
+            <div>
               <input
                 type="text"
                 name="username"
                 value={formData.username}
                 onChange={handleChange}
                 placeholder="Username"
-                className="w-full rounded-md border p-2"
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 required
               />
             </div>
-            <div className="mb-4">
+            <div>
               <input
                 type="text"
                 name="firstname"
                 value={formData.firstname}
                 onChange={handleChange}
                 placeholder="First Name"
-                className="w-full rounded-md border p-2"
+                className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 required
               />
             </div>
-            <div className="mb-4">
+            <div>
               <input
                 type="text"
                 name="lastname"
                 value={formData.lastname}
                 onChange={handleChange}
                 placeholder="Last Name"
-                className="w-full rounded-md border p-2"
+                className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 required
               />
             </div>
-            <div className="mb-4">
+            <div>
               <input
                 type="email"
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
                 placeholder="Email"
-                className="w-full rounded-md border p-2"
+                className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 required
               />
             </div>
-            <div className="mb-4">
+            <div>
               <input
                 type="password"
                 name="password"
                 value={formData.password}
                 onChange={handleChange}
                 placeholder="Password"
-                className="w-full rounded-md border p-2"
+                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 required
               />
             </div>
-            <div className="text-center">
-              <button
-                type="submit"
-                className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-              >
-                Create Account
-              </button>
-            </div>
           </div>
-        </div>
-      </form>
+          <div>
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <FontAwesomeIcon icon={faRightFromBracket} />
+              </span>
+              Create Account
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
