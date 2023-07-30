56b4ecc7aeb37a66cd03a6a2856cdb1ed91431b5
Rahmin <rahminshoukoohi@gmail.com>
2023-07-30 02:32:58 -0400

    created ternary navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../hooks/reduxTypescriptTypes";
 
 const Navbar = () => {
+  const userAuth = useAppSelector((state) => state.userAuth);
+
   return (
     <nav className="bg-blue-500 p-4">
       <div className="flex items-center justify-between">
@@ -10,20 +13,45 @@ const Navbar = () => {
             Home
           </Link>
         </div>
-        <div className="flex items-center space-x-4">
-          <Link
-            to="/create-account"
-            className="text-white hover:text-blue-200 hover:underline"
-          >
-            Create Account
-          </Link>
-          <Link
-            to="/login"
-            className="text-white hover:text-blue-200 hover:underline"
-          >
-            Login
-          </Link>
-        </div>
+        {userAuth.isLoggedIn ? (
+          <div className="flex items-center space-x-4">
+            <span className="text-white">Welcome, {userAuth.first_name}</span>
+            <Link
+              to="/feature-1"
+              className="text-white hover:text-blue-200 hover:underline"
+            >
+              Feature 1
+            </Link>
+            <Link
+              to="/feature-2"
+              className="text-white hover:text-blue-200 hover:underline"
+            >
+              Feature 2
+            </Link>
+            {/* Add more links as needed */}
+            <Link
+              to="/logout"
+              className="text-white hover:text-blue-200 hover:underline"
+            >
+              Logout
+            </Link>
+          </div>
+        ) : (
+          <div className="flex items-center space-x-4">
+            <Link
+              to="/create-account"
+              className="text-white hover:text-blue-200 hover:underline"
+            >
+              Create Account
+            </Link>
+            <Link
+              to="/login"
+              className="text-white hover:text-blue-200 hover:underline"
+            >
+              Login
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
